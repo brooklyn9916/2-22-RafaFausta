adf01dec06b056426898040280870e470deeec15
brooklyn9916 <rafaspumante@gmail.com>
2022-01-24 09:11:04 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Rafa Fausta | About</title>
+</head>
+<body>
+    <h1> Halaman About</h1>
+    <h3> <?= $nama; ?></h3>
+    <p> <?= $email; ?></p>
+    <img src="images/rafa.jpeg" alt="<?= $nama; ?>" width="400px" height ="200">
+</body>
+</html>
