d82ba9e8ef272907c063daeefedda9978a693eaa
brooklyn9916 <rafaspumante@gmail.com>
2022-02-07 09:03:21 +0700

    update modul 3

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -13,6 +13,9 @@
         <li class="nav-item">
           <a class="nav-link {{ ($title === 'Gallery') ? 'active' : ''}}" href="/gallery">Galerry</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link {{ ($title === 'Contacts') ? 'active' : ''}}" href="/contacts">Contact Us</a>
+        </li>
       </ul>
       
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
 
 
 
@@ -24,3 +25,11 @@ Route::get('/gallery', function () {
         "title" => "Gallery"
     ]);
 });
+
+Route::get('/contacts', function(){
+    return view ('contacts', [
+        "title" => "contacts"
+    ]);
+});
+
+Route::resource('/contacts', ContactController::class);
